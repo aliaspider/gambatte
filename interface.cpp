dea5a4acbff59507ed30237b5ae6f61e4620bc8b
aliaspider <aliaspider@gmail.com>
2017-10-07 03:32:40 +0100

    misc

--- interface.cpp
+++ interface.cpp
@@ -1,29 +1,35 @@
 
-#include "interface.h"
+#include "../interface.h"
 #include "gambatte.h"
 
 gambatte::GB gameboy;
 
 void module_init(const module_init_info_t *init_info, module_info_t *module_info)
 {
-   gameboy.load(init_info->filename);
+   if (gameboy.load(init_info->filename) < 0)
+      debug_log("load error\n");
+
    module_info->output_width = 160;
    module_info->output_height = 144;
    module_info->screen_format = ARGB8888;
    module_info->stereo = true;
    module_info->framerate = 60;
    module_info->audio_samples_per_frame = 35112;
+
+   debug_log("module init\n");
 }
 
 void module_destroy()
 {
+   debug_log("module destroy\n");
+
 
 }
 
 void module_run(module_run_info_t *run_info)
 {
    std::size_t max_samples = run_info->max_samples;
-   gameboy.runFor(run_info->screen.u32, run_info->pitch, run_info->sound_buffer.u32, max_samples);
+   std::ptrdiff_t samples = gameboy.runFor(run_info->screen.u32, run_info->pitch, run_info->sound_buffer.u32, max_samples);
    run_info->max_samples = max_samples;
-
+   run_info->frame_completed = samples > 0;
 }
